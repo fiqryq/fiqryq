a1c5f8f14d1a996af17c9eac4bfba7848d04891e
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-02-15 20:32:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const readme = `\
 - 🎹 I like music. 
 - 📫 How to reach me: DM me at instagram [@fiqryq](https://instagram.com/fiqryq)
 - 📱 Visit my [playstore](https://tinyurl.com/suncodeid)
+- 🔗 Visit my [stackoverflow](https://stackoverflow.com/users/12970384/fiqry-choerudin?tab=profile)
 - ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 - ⏰ Updated on ${new Date().toUTCString()}
 
